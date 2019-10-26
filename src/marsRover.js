a52a2f8e9a0027a0558cb844f3f2a8ef581d7268
jmotosp <jmotosp@gmail.com>
2019-10-26 18:04:30 +0200

    add @ts-check & refactor to addapt it

--- src/marsRover.js
+++ src/marsRover.js
@@ -1,57 +1,71 @@
+//@ts-check
 class MarsRover {
-    constructor(direction) {
-        this.direction = direction || DIRECTIONS.North;
-    }
-    sendCommand(command) {
-        if (command === COMMANDS.Left) {
-            return new MarsRover(this.direction.turnLeft());
-        }
-        return new MarsRover(this.direction.turnRight());
-    }
-};
-
-const DIRECTIONS = {},
-    COMMANDS = {
-        Left: 'L', Right: 'R'
-    };  
-
-class Direction {
-    constructor(name) {
-        this.name = name;
-    }
-    toString() {
-        return this.name;
-    }
-    setTurns(left, right) {
-        this.left = left;
-        this.right = right;
-    }
-    turnRight() {
-        return this.right;
-    }
-    turnLeft() {
-        return this.left;
-    }
+	/**
+	 * @param {Direction} direction
+	 */
+	constructor(direction) {
+		this.direction = direction || DIRECTIONS.North;
+	}
+	/**
+	 * @param {String} command
+	 */
+	sendCommand(command) {
+		if (command === COMMANDS.Left) {
+			return new MarsRover(this.direction.turnLeft());
+		}
+		return new MarsRover(this.direction.turnRight());
+	}
 }
 
-(function initializeDirectionsFactory() {
-    function left(left) { return left; }
-    function right(right) { return right; }
+class Direction {
+	/**
+	 * @param {String} name
+	 */
+	constructor(name) {
+		this.name = name;
+	}
+	toString() {
+		return this.name;
+	}
+	/**
+	 * @param {Direction} left
+	 * @param {Direction} right
+	 */
+	setTurns(left, right) {
+		this.left = left;
+		this.right = right;
+	}
+	turnRight() {
+		return this.right;
+	}
+	turnLeft() {
+		return this.left;
+	}
+}
 
-    DIRECTIONS.North = new Direction("North");
-    DIRECTIONS.West = new Direction("West");
-    DIRECTIONS.South = new Direction("South");
-    DIRECTIONS.East = new Direction("East");
-    DIRECTIONS.North.setTurns(left(DIRECTIONS.East), right(DIRECTIONS.West));
-    DIRECTIONS.West.setTurns(left(DIRECTIONS.North), right(DIRECTIONS.South));
-    DIRECTIONS.South.setTurns(left(DIRECTIONS.West), right(DIRECTIONS.East));
-    DIRECTIONS.East.setTurns(left(DIRECTIONS.South), right(DIRECTIONS.North));
-})()
+const DIRECTIONS = {
+		North: new Direction('North'),
+		West: new Direction('West'),
+		South: new Direction('South'),
+		East: new Direction('East')
+	},
+	COMMANDS = {
+		Left: 'L',
+		Right: 'R'
+	};
+
+(function initializeDirectionsFactory() {
+	DIRECTIONS.North.setTurns(DIRECTIONS.East, DIRECTIONS.West);
+	DIRECTIONS.West.setTurns(DIRECTIONS.North, DIRECTIONS.South);
+	DIRECTIONS.South.setTurns(DIRECTIONS.West, DIRECTIONS.East);
+	DIRECTIONS.East.setTurns(DIRECTIONS.South, DIRECTIONS.North);
+})();
 
 Object.freeze(DIRECTIONS);
 
 module.exports = {
-    MarsRover: MarsRover,
-    DIRECTIONS: DIRECTIONS,
-    COMMANDS: COMMANDS
+	MarsRover: MarsRover,
+	DIRECTIONS: DIRECTIONS,
+	COMMANDS: COMMANDS,
+	Direction: Direction
 };
